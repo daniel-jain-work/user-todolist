cd130e4b2839c704f11ebc63b78a3accc659ecc0
Daniel Jain <daniel4352de@gmail.com>
2021-03-11 11:18:35 -0500

    update useEffect function on App

--- src/App.js
+++ src/App.js
@@ -8,20 +8,23 @@ const API_URL = 'https://jsonplaceholder.typicode.com/users';
 export default function App() {
   const [usersList, setUsersList] = useState([]);
 
-  useEffect(async () => {
+  const loadResource = async () => {
     const users = await fetch(API_URL).then(res => res.json());
-    
     setUsersList(users.map(user => ({
       ...user,
       age: generateAge(),
     })));
+  };
+
+  useEffect(() => {
+    loadResource();
   }, []);
 
   const handleUpdateName = (id, name) => {
     const newList = [...usersList];
     const index = newList.findIndex(user => user.id === id);
     if (index > -1) {
-      newList[index].name = name; // Possible
+      newList[index].name = name;
       setUsersList([...newList]);
     }
   };
